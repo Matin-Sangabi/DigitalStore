888a9256e2761f7b0079500eea7e114108c32723
Matin-Sangabi <89559512+Matin-Sangabi@users.noreply.github.com>
2022-09-03 23:13:07 +0430

    filterProducts was added

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,5 +1,5 @@
 import Layout from "../layout/layout";
-import { Link, NavLink, useSearchParams } from "react-router-dom";
+import { NavLink, useSearchParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useProducts } from "../provider/productsProvider";
 import { Disclosure } from "@headlessui/react";
@@ -7,6 +7,7 @@ import {
   HiOutlineViewGrid,
   HiOutlineDeviceTablet,
   HiChevronDown,
+  HiOutlineSortDescending,
 } from "react-icons/hi";
 import {
   IoPhonePortraitOutline,
@@ -30,48 +31,19 @@ const ProductsPage = () => {
       setFilterProducts(products);
     }
   }, [products, productsCategories]);
-  console.log(filterProducts);
   return (
     <Layout>
-      <div className="container mx-auto max-w-screen-xl px-4 grid grid-cols-12 grid-rows-[55px_minmax(500px,_1fr)] md:gap-8 pt-36">
+      <div className="container mx-auto max-w-screen-xl px-4 grid grid-cols-12 grid-rows-[55px_minmax(500px,_1fr)] md:gap-8 pt-32">
         <SortSection />
-        <div className="col-span-12 md:col-span-8 lg:col-span-9 ">
+        <div className="col-span-12 md:col-span-8 lg:col-span-9 bg-gray-300 shadow-md rounded-md hidden md:flex items-center p-4">
+          <div className="py-2 p-2 bg-cyan-900 text-slate-100 rounded-md">
+            <HiOutlineSortDescending className="w-5 h-5" />
+          </div>
+          <FilterProductsList filterProducts = {filterProducts} setFilterProducts={setFilterProducts}/>
+        </div>
+        <div className="col-span-12 md:col-span-8 lg:col-span-9 mt-8">
           <section className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4  mx-auto gap-8 px-4 ">
-            {filterProducts.map((item, index) => {
-              return (
-                <div
-                  className="bg-gray-300 shadow-md rounded-md flex flex-col p-2 gap-8"
-                  key={index}
-                >
-                  <div className="w-20 md:w-28 h-16 md:h-20 flex justify-center items-center bg-opacity-80 bg-cyan-900 rounded-tr-3xl rounded-br-3xl rounded-tl-xl shadow-xl  mx-auto mb-8">
-                    <img
-                      src={require(`./../assets/images/${item.image[0].path}`)}
-                      alt={item.name}
-                      className="max-w-full h-auto object-cover rotate-12"
-                    />
-                  </div>
-                  <div className="flex flex-col items-start">
-                    <h1 className="font-bold text-xs md:text-base xl:text-sm  text-cyan-900">
-                      {item.name}
-                    </h1>
-                    <h1 className="text-gray-400">{item.categories}</h1>
-                    <div className="flex w-full justify-between mt-4 items-center">
-                      <h1 className="text-sm md:text-base text-yellow-600 font-bold ">
-                        $ {item.price}
-                      </h1>
-                      <Link
-                        to={`/product/${item._id}`}
-                        className="w-8 h-8 bg-cyan-900 rounded-full text-slate-100 flex justify-center items-center text-lg group transition-all ease-linear duration-500 "
-                      >
-                        <span className="group-hover:rotate-[360deg] transition-all ease-in-out duration-500">
-                          +
-                        </span>
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
+            <ProductsList filterProducts={filterProducts} />
           </section>
         </div>
       </div>
@@ -85,7 +57,7 @@ const SortProducts = [
     title: "Categories",
     content: [
       { name: "All", path: "", icon: () => <HiOutlineViewGrid /> },
-      { name: "IPhone", path: "phone", icon: () => <IoPhonePortraitOutline/> },
+      { name: "IPhone", path: "phone", icon: () => <IoPhonePortraitOutline /> },
       { name: "MacBook", path: "Mac", icon: () => <IoLaptopOutline /> },
       { name: "AppleWatch", path: "watch", icon: () => <IoWatchOutline /> },
       { name: "AirPod", path: "air pod", icon: () => <IoRecordingOutline /> },
@@ -113,28 +85,37 @@ const SortSection = () => {
               <Disclosure defaultOpen>
                 {({ open }) => (
                   <>
-                    <Disclosure.Button  className="w-full flex justify-between items-center border-b-2 py-2 border-gray-600 text-gray-800">
+                    <Disclosure.Button className="w-full flex justify-between items-center border-b-2 py-2 border-gray-600 text-gray-800">
                       <h1 className="font-bold text-lg">{product.title}</h1>
                       <HiChevronDown
                         className={`${
-                          open 
+                          open
                             ? "rotate-180 transform transition-all ease-in-out duration-500"
-                            : ""
+                            : "rotate-0 transition-all ease-in-out duration-500"
                         }  h-5 w-5 text-gray-800`}
                       />
                     </Disclosure.Button>
                     <Disclosure.Panel className="pt-4 pb-2 text-gray-700">
                       <ul
-                        className={"mt-4 flex flex-col gap-2 mb-6 transition-all ease-in-out duration-500"}
+                        className={
+                          "mt-4 flex flex-col gap-2 mb-6 transition-all ease-in-out duration-500"
+                        }
                       >
                         {product.content.map((item, index) => {
                           return (
-                            <li key={index} className="flex gap-x-2 items-center">
-                              {item.icon && <span className="p-2 rounded-full bg-cyan-900 bg-opacity-75 text-lg text-gray-200">{item.icon()}</span>}
+                            <li
+                              key={index}
+                              className="flex gap-x-2 items-center"
+                            >
+                              {item.icon && (
+                                <span className="p-2 rounded-full bg-gray-700 bg-opacity-75 text-lg text-gray-200">
+                                  {item.icon()}
+                                </span>
+                              )}
                               <NavLink
                                 to={`/products?cat=${item.path}`}
                                 className={
-                                  "block p-2 w-full hover:bg-cyan-900 transition-all ease-in-out duration-500 rounded-md text-gray-900 hover:text-gray-200"
+                                  "block p-2 w-full hover:bg-gray-500 transition-all ease-in-out duration-500 rounded-md text-gray-900 hover:text-gray-200"
                                 }
                               >
                                 {item.name}
@@ -151,6 +132,82 @@ const SortSection = () => {
           );
         })}
       </div>
+    </div>
+  );
+};
+
+const ProductsList = ({ filterProducts }) => {
+  return (
+    <>
+      {filterProducts.map((item, index) => {
+        return (
+          <NavLink
+            to={`/product/${item._id}`}
+            className="bg-gray-300 shadow-md rounded-md flex flex-col p-2 gap-8 group hover:shadow-2xl hover:shadow-gray-800 transition-all ease-in-out duration-300"
+            key={index}
+          >
+            <div className="w-20 md:w-28 h-16 md:h-20 flex justify-center items-center group-hover:py-2 bg-opacity-80 bg-cyan-900 rounded-tr-3xl rounded-br-3xl rounded-tl-xl shadow-xl  mx-auto mb-8">
+              <img
+                src={require(`./../assets/images/${item.image[0].path}`)}
+                alt={item.name}
+                className="max-w-full h-auto object-cover rotate-12"
+              />
+            </div>
+            <div className="flex flex-col items-start">
+              <h1 className="font-bold text-xs md:text-base xl:text-sm  text-cyan-900">
+                {item.name}
+              </h1>
+              <h1 className="text-gray-400">{item.categories}</h1>
+              <div className="flex w-full justify-between mt-4 items-center">
+                <h1 className="text-sm md:text-base text-yellow-600 font-bold ">
+                  $ {item.price}
+                </h1>
+                <button className="w-8 h-8 bg-cyan-900 rounded-full text-slate-100 flex justify-center items-center text-lg group transition-all ease-linear duration-500 ">
+                  <span className="group-hover:rotate-[360deg] transition-all ease-in-out duration-500">
+                    +
+                  </span>
+                </button>
+              </div>
+            </div>
+          </NavLink>
+        );
+      })}
+    </>
+  );
+};
+
+const FilterProductsList = ({filterProducts , setFilterProducts}) => {
+  const filters = [
+    {name : "Most Popular" , value : "popular"},
+    {name : "Most Visited" , value : "Visited"},
+    {name : "the most expensive" , value : "expensive"},
+    {name : "The cheapest" , value : "cheapest"},
+  ];
+  const filteredProductsListItem = (e) =>{
+    switch(e.target.value){
+      case 'expensive' : {
+        const sortFilter = [...filterProducts].sort((a ,b)=>{
+          return a.price > b.price ? -1 : 1;
+        })
+        return setFilterProducts(sortFilter)
+      }
+      case 'cheapest' : {
+        const sortFilter = [...filterProducts].sort((a ,b)=>{
+          return a.price > b.price ? 1 : -1;
+        })
+        return setFilterProducts(sortFilter);
+      }
+    }
+  }
+  return (
+    <div className="flex items-center gap-x-8 px-4">
+      {filters.map((item , index)=>{
+        return(
+          <button key={index} value={item.value} onClick={filteredProductsListItem} type="button" className="font-semibold text-gray-400 cursor-pointer hover:border-b-2 hover:border-gray-600 hover:py-2 transition-all ease-in-out duration-500">
+            {item.name}
+          </button>
+        )
+      })}
     </div>
   );
 };
