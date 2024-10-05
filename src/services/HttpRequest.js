13c9d85fb874861375b5928e1b46c1349d3c1a1c
matin-sangabi <sangabiwork@gmail.com>
2024-10-05 14:51:46 +0330

    fix endpoint url

--- src/services/HttpRequest.js
+++ src/services/HttpRequest.js
@@ -3,7 +3,7 @@ import axios from "axios";
 axios.defaults.baseURL =
   process.env.NODE_ENV === "development"
     ? "https://journal-trade-jkrq.vercel.app/api"
-    : "https://digitalnodestore.iran.liara.run/api";
+    : "https://journal-trade-jkrq.vercel.app/api";
 
 export const http = {
   get: axios.get,
